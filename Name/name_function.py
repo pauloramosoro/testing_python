566f6a17d0b659a1510acc33d8a0e4ceb33ad5e4
unknown <pauloramosoro86@gmail.com>
2021-10-23 15:14:14 -0500

    Added and fixed age problem

--- Name/name_function.py
+++ Name/name_function.py
@@ -1,8 +1,11 @@
-def hello(name) :
-    viruatl_wave = print(f"Hi my name is {name}")
+def hello(name, age) :
+    my_age = age * 100
+    my_age = int(my_age)
+    viruatl_wave = print(f"Hi my name is {name} and I am {my_age} years old")
 
     return viruatl_wave
 
 name = 'Jacob'
+age = 0.35
 
-hello(name)
+hello(name, age)
